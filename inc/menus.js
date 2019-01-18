479571dbc9405a40ae8018d6225f1eacae15fc10
Ivan Assuncao <ivan_assuncao@hotmail.com>
2019-01-18 13:18:58 -0200

    Versão Inicial sem bower

--- inc/menus.js
+++ inc/menus.js
@@ -20,38 +20,43 @@ module.exports = {
 
         fields.photo = `images/${path.parse(files.photo.path).base}`
 
-        let query, params
+        let query, queryPhoto = '' ,params = [
+                                fields.title,
+                                fields.description,
+                                fields.price
+
+                            ]
+
+        if(files.photo){
+            queryPhoto =  ',photo = ?'
+
+            params.push(  fields.photo)
+        }                    
 
         if(parseInt(fields.id) > 0)
         {
+            params.push(  fields.id)
+
             query = `
                 update tb_menus
                     set title = ?,
                     description = ?,
                     price = ?,
-                    photo = ?
+                    ${queryPhoto}
                 where id = ?
-            `
-            params = [
-                    fields.title,
-                    fields.description,
-                    fields.price,
-                    fields.photo,
-                    fields.id
-                ]
+            `,params
         }
         else
         {
+
+            if(!files.photo){
+                reject('Envie a foto...')
+            }
+
             query = `
-            insert into tb_menus(title,description,price,photo)
+            insert into tb_menus(title,description,price ${queryPhoto})
             values(?,?,?,?)
-            `
-            params = [
-                fields.title,
-                fields.description,
-                fields.price,
-                fields.photo
-            ]
+            `,params
         }
 
         return new Promise((resolve,reject)=>{
